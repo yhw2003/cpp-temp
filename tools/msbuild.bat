e377e77ce00d7a8f5f0fe814a1a2e305add74936
yhw2003 <321612813@qq.com>
2025-03-08 15:31:18 +0800

    modified:   CMakePresets.json 	modified:   tools/msbuild.bat

--- tools/msbuild.bat
+++ tools/msbuild.bat
@@ -1,2 +1,3 @@
 @Echo off
+Echo "Warning: If the obj file has been generated, the compile_commands.json file will be generated."
 MSBuild.exe -logger:"C:\Users\y\Develop\qt-demo\tools\lib\CompileCommandsJson.dll" %*
